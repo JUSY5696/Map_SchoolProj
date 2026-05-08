ef36ad539add0721ebd381cbdc73503bc0b96d24
Jusy <138380535+JUSY5696@users.noreply.github.com>
2026-05-08 15:43:58 +0900

    Set page title and add loop for asterisks

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 
-st.header("Maps")
+st.set_page_config(page_title="NDHS Maps")
+for i in range(10) :
+  print('*' * (i+1))
+st.header("NDHS Maps")
