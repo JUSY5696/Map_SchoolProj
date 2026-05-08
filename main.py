2ec81d7dc1460d36c0e07202791c594a098ad246
Jusy <138380535+JUSY5696@users.noreply.github.com>
2026-05-08 16:15:52 +0900

    Implement map visualization with folium markers
    Added map visualization with folium markers and updated data reading.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 
+#streamlit text styling
 st.set_page_config(page_title="NDHS Maps", layout="wide")
 
 st.title("NDHS Maps")
@@ -19,7 +20,20 @@ st.caption("Caption")
 st.code("for i in range(5) : \n  print('*' * (i + 1))")
 st.code("* \n** \n*** \n**** \n*****")
 
+#read data(CSV)
 df = pd.read_csv('인천광역시 남동구_고등학교_20240325.csv', encoding='cp949')
 df_latlon = df[['위도', '경도']]
 df_latlon = df_latlon.rename(columns={'위도' : 'lat', '경도' : "lon"})
-st.map(df_latlon)
+#st.map(df_latlon)
+
+#Maps with Marker(Map Visualization Step)
+m = folium.Map(
+    location=[37.40583317, 126.7214872],
+    zoom_start=12
+  )
+
+folium.Marker(
+  location = [37.40583317, 126.7214872],
+  tooltip = "Click Here",
+  popup = "NDHS"
+).add_to(m)
