04cdd99e1b6d7ec700057cc4ac5db6a665b394a6
Jusy <138380535+JUSY5696@users.noreply.github.com>
2026-05-08 16:01:38 +0900

    Update headers and add new markdown elements

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import pandas as pd
 st.set_page_config(page_title="NDHS Maps", layout="wide")
 
 st.title("NDHS Maps")
-st.header("2026 NDHS Hiking Event Guide Map")
-st.markdown("This hiking course guide was made by me and our team.")
-st.markdown("Choose the course from the left menu and join the event.")
+st.markdown("## 2026 NDHS Hiking Event Guide Map")
 
 st.markdown("# big title")
 st.markdown("## small title")
 st.markdown("**Bold**")
 st.markdown("*Italic*")
+
+st.header("Header")
+st.subheader("Subheader")
+st.caption("Caption")
+st.code("for i in range(5) :")
