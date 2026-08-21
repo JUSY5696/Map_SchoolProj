5e489dc18f1676b203c4da228f189b71f36d2d16
Jusy <138380535+JUSY5696@users.noreply.github.com>
2026-08-21 15:47:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+import os
 
 #streamlit text styling
 st.set_page_config(page_title="NDHS Maps", layout="wide")
